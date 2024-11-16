04985a89e87cd0a09b46044fcaf4b6fa0bc0b7d1
Carlos Gasca <l21121632@morelia.tecnm.mx>
2024-11-16 14:36:43 -0600

    avances
    yastoy harto

---
+++ src/app/components/BannerPrecisionMedicine/bannerPrecisionMedicine.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+
+const bannerPrecisionMedicine = () => {
+  return (
+    <div className="bg-blue-900 text-white py-12">
+    <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 px-6 items-center">
+      {/* Columna de texto */}
+      <div>
+        <h2 className="text-4xl font-bold mb-6">Medicina de Precisión</h2>
+        <p className="mb-6 text-lg">
+          La medicina de precisión ha tenido un impacto significativo en la práctica clínica de oncología, brindando diversos aportes que han mejorado la atención a los pacientes con cáncer.
+        </p>
+        <p className="mb-6 text-lg">
+          En FICMAC, ofrecemos un enfoque eficiente y efectivo para el cuidado de la salud, con beneficios significativos tanto para los pacientes como para la comunidad médica y científica:
+        </p>
+        <ul className="list-disc list-inside space-y-2 text-lg">
+          <li>Diagnóstico más preciso</li>
+          <li>Terapias dirigidas</li>
+          <li>Selección de tratamientos personalizados</li>
+          <li>Monitorización del progreso del tratamiento</li>
+          <li>Pronóstico y prevención</li>
+          <li>Investigación y desarrollo de nuevos tratamientos</li>
+        </ul>
+      </div>
+
+      {/* Columna de video o imagen */}
+      <div className="relative">
+      <iframe
+          width="640"
+          height="400"
+          src="https://www.youtube.com/embed/tLh1j-bKn-s?si=ECcwgVQdPKExm7wa"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default bannerPrecisionMedicine;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
-'use client'
+"use client";
 
 import Navbar from "./components/Navbar/navbar";
+import ImageSlider from "./components/ImageSlider/imageSlider";
+import Statistics from "./components/Statistics/statistics";
+import BannerPrecisionMedicine from "./components/BannerPrecisionMedicine/bannerPrecisionMedicine";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="bg-[#F5F5F5] w-full h-svh">
-        <Navbar />
+      <Navbar />
+      <ImageSlider />
+      <Statistics />
 
+      <div className="flex flex-col items-center justify-center py-16 bg-white">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">
+          Recibe información de tu interés
+        </h1>
+        <p className="text-lg text-gray-700 mb-8 text-center">
+          Complete el formulario para unirse a nuestra comunidad en línea y
+          seguir sumando años de vida en la lucha contra el cáncer.
+        </p>
+        <Link
+          href="/register"
+          className="bg-[#547EED] w-auto text-white py-2 px-4 rounded-full hover:bg-blue-600 transition duration-300"
+        >
+          ¡Regístrate aquí!
+        </Link>
+      </div>
+
+      <BannerPrecisionMedicine />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
