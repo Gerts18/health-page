3188a4d3124a02aaa0a7ae64513927a3e9f673a8
AdrianFlV324 <floresvegadiegoadrian@gmail.com>
2024-11-18 13:45:52 -0600

    Subida de archivos login

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Login from "../components/Login/index"
+import '../components/Login/login.css'
+
+const LoginPage = () => {
+  return (
+    <main>
+        <Login />
+    </main>
+  )
+}
+
+export default LoginPage
