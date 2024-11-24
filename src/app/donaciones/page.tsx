7203746132ae6baad8c4e571e80e5adaa2debadd
meketone <meketone17@gmail.com>
2024-11-23 18:22:19 -0600

    creo que va bien
    El header tapa el resto pero se ve bien}

--- src/app/donaciones/page.tsx
+++ src/app/donaciones/page.tsx
@@ -1,28 +1,47 @@
-'use client'
+"use client";
 
-import React from 'react'
-
-import { useRouter } from 'next/navigation';
+import React from "react";
+import Header from "../components/Header/Index";
+import Footer from "../components/Footer/index";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const PaginaDonaciones = () => {
+  const router = useRouter();
 
-    
-    const router = useRouter();
+  return (
+    <div className="min-h-screen flex flex-col bg-white">
+      <Header />
 
+      <div>
+        <div className="relative w-full h-64 mt-4">
+          <p>
+            Brindarle calidad de vida a los pacientes es posible, gracias al
+            proceso de transformación en la precisión del tratamiento del
+            cáncer. Sigamos sumando años de vida en la lucha contra el
+            cáncer...Apóyanos
+          </p>
 
-    return (
-        <>
-        <div>Pagina de donciones de mike</div>
-        
-        <button 
-        className='border-medium'
+          <Image
+            src="/assets/PreguntasFrecuentes.jpg"
+            alt="Description of image"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
+      </div>
+      <button
+        className="border-medium mt-4"
         onClick={() => {
-            router.push('/pagoDonacion');
-        } }
-         >Donar</button>
+          router.push("/pagoDonacion");
+        }}
+      >
+        Donar
+      </button>
+      <Footer />
+    </div>
+  );
+};
 
-        </>
-    )
-}
-
-export default PaginaDonaciones
+export default PaginaDonaciones;
