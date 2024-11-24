2ec9e3385f9027dc5eb0bbf4ceece57fc137a06f
AdrianFlV324 <floresvegadiegoadrian@gmail.com>
2024-11-24 16:57:58 -0600

    header y footer implementados

--- src/app/components/about/index.tsx
+++ src/app/components/about/index.tsx
@@ -1,13 +1,17 @@
+
 import React from 'react';
 import AboutVideoArea from './MissionVission';
 import AboutCounterArea from './Specialties';
 import TeamAreaHomeOne from './Specialists';
-import Carrousel from './Carousel'
-import Awards from './Awards'
+import Carrousel from './Carousel';
+import Awards from './Awards';
+import Header from '../Header/index';
+import Footer from '../Footer/index';
 
 const About = () => {
   return (
     <>
+      <Header />
       <main>
         <Carrousel/>
         <AboutVideoArea />
@@ -15,6 +19,7 @@ const About = () => {
         <TeamAreaHomeOne />
         <Awards />
       </main>
+      <Footer />
     </>
   );
 };
