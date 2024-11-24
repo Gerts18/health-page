d8f3a26bbc94e53bc524373d6d9637dbf934eba6
meketone <meketone17@gmail.com>
2024-11-24 12:05:03 -0600

    falta la seccion de apoyo

--- src/app/donaciones/page.tsx
+++ src/app/donaciones/page.tsx
@@ -28,15 +28,8 @@ const PaginaDonaciones = () => {
             />
           </div>
         </div>
-        <button
-          className="border-medium mt-4"
-          onClick={() => {
-            router.push("/pagoDonacion");
-          }}
-        >
-          Donar
-        </button>
-        <section>
+        
+        <section className="don_1">
           <div>
             <Image
               src="/assets/don_1.jpg"
@@ -93,7 +86,12 @@ const PaginaDonaciones = () => {
                       eros risus donec cras massa in ut elementum. Sed facilisis
                       pretium integer felis a.
                     </p>
-                    <button className="text-white bg-[#EB356E] w-40 rounded-3xl font-medium text-center">
+                    <button
+                      className="text-white bg-[#EB356E] w-40 rounded-3xl font-medium text-center"
+                      onClick={() => {
+                        router.push("/pagoDonacion");
+                      }}
+                    >
                       CONOCE MÁS →
                     </button>
                   </div>
@@ -111,6 +109,101 @@ const PaginaDonaciones = () => {
               referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen
             ></iframe>
+          </div>
+        </section>
+        <section className="don_2">
+          <div>
+            <Image
+              src="/assets/don_2.jpg"
+              alt="Description of image"
+              layout="responsive"
+              width={400}
+              height={200}
+              quality={100}
+            />
+
+            <h1 className="titular-Black">¿QUIERES APOYAR?</h1>
+            <p className="titular-Bold">¿Cómo Donar?</p>
+            <p className="texto_don">
+              Cualquier persona puede donar de forma fácil, rápida y segura.
+            </p>
+            <p className="titular-Bold">Donaciones presenciales:</p>
+            <p className="texto_don">Datáfono</p>
+            <p className="titular-Bold">Donaciones transferencia bancaria:</p>
+            <p className="texto_medium">Bancolombia Ahorros No. 40269058686</p>
+            <p className="texto_don">
+              A nombre de Fundación para la Investigación Clínica y Molecular
+              Aplicada del Cáncer FICMAC Nit. 900239016-1
+            </p>
+            <button
+              className="text-white bg-[#EB356E] w-60 rounded-3xl font-medium text-center"
+              onClick={() => {
+                window.location.href ="https://sites.placetopay.com/ficmac_abierto";
+              }}
+            >
+              ¡Haz tu donación aquí!
+            </button>
+          </div>
+        </section>
+
+        <section>
+          <h1>Gracias a ti y a las donaciones hemos ayudado a:</h1>
+          <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-6">
+            <div className="flex flex-col items-center text-center">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 448 512"
+                className="text-blue-500 text-5xl mb-4"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zM104 424c0 13.3 10.7 24 24 24s24-10.7 24-24-10.7-24-24-24-24 10.7-24 24zm216-135.4v49c36.5 7.4 64 39.8 64 78.4v41.7c0 7.6-5.4 14.2-12.9 15.7l-32.2 6.4c-4.3.9-8.5-1.9-9.4-6.3l-3.1-15.7c-.9-4.3 1.9-8.6 6.3-9.4l19.3-3.9V416c0-62.8-96-65.1-96 1.9v26.7l19.3 3.9c4.3.9 7.1 5.1 6.3 9.4l-3.1 15.7c-.9 4.3-5.1 7.1-9.4 6.3l-31.2-4.2c-7.9-1.1-13.8-7.8-13.8-15.9V416c0-38.6 27.5-70.9 64-78.4v-45.2c-2.2.7-4.4 1.1-6.6 1.9-18 6.3-37.3 9.8-57.4 9.8s-39.4-3.5-57.4-9.8c-7.4-2.6-14.9-4.2-22.6-5.2v81.6c23.1 6.9 40 28.1 40 53.4 0 30.9-25.1 56-56 56s-56-25.1-56-56c0-25.3 16.9-46.5 40-53.4v-80.4C48.5 301 0 355.8 0 422.4v44.8C0 491.9 20.1 512 44.8 512h358.4c24.7 0 44.8-20.1 44.8-44.8v-44.8c0-72-56.8-130.3-128-133.8z"></path>
+              </svg>
+              <h3 className="text-3xl font-bold text-[#EB356E]">22,000</h3>
+              <p className="text-gray-600 mt-2">
+                {" "}
+                Muestras actuales en el biobanco{" "}
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 640 512"
+                className="text-blue-500 text-5xl mb-4"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M72 64h24v240c0 44.1 35.9 80 80 80s80-35.9 80-80V64h24c4.4 0 8-3.6 8-8V8c0-4.4-3.6-8-8-8H72c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8zm72 0h64v96h-64V64zm480 384H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h608c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zM360 64h24v240c0 44.1 35.9 80 80 80s80-35.9 80-80V64h24c4.4 0 8-3.6 8-8V8c0-4.4-3.6-8-8-8H360c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8zm72 0h64v96h-64V64z"></path>
+              </svg>
+              <h3 className="text-3xl font-bold text-[#EB356E]">92,000</h3>
+              <p className="text-gray-600 mt-2">
+                Muestras actuales en seroteca
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 576 512"
+                className="text-blue-500 text-5xl mb-4"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M572.694 292.093L500.27 416.248A63.997 63.997 0 0 1 444.989 448H45.025c-18.523 0-30.064-20.093-20.731-36.093l72.424-124.155A64 64 0 0 1 152 256h399.964c18.523 0 30.064 20.093 20.73 36.093zM152 224h328v-48c0-26.51-21.49-48-48-48H272l-64-64H48C21.49 64 0 85.49 0 112v278.046l69.077-118.418C86.214 242.25 117.989 224 152 224z"></path>
+              </svg>
+              <h3 className="text-3xl font-bold text-[#EB356E]">9,000</h3>
+              <p className="text-gray-600 mt-2">
+                Muestras actuales en la biblioteca
+              </p>
+            </div>
           </div>
         </section>
 
