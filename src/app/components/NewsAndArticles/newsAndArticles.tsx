226322f4b31f6aa4dc2ae51647966d7c817ebcb3
meketone <meketone17@gmail.com>
2024-12-07 12:26:27 -0600

    Empezamos

--- src/app/components/NewsAndArticles/newsAndArticles.tsx
+++ src/app/components/NewsAndArticles/newsAndArticles.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const newsAndArticles = () => {
   return (
     <div className="py-10">
-      <h1 className="text-4xl px-28 pb-4 font-bold">Noticias y artículos</h1>
+      <h1 className="text-4xl px-28 pb-4 font-bold">Noticias</h1>
       <h2 className="text-2xl px-28 pb-4 font-bold text-[#EB356E]">
         Obtenga todas las actualizaciones aquí.
       </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 px-16 pb-8">
-        <div className="border rounded-lg overflow-hidden shadow-lg">
+      <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="border rounded-lg overflow-hidden shadow-lg p-3">
           <div className="rounded-lg">
             <Image
-              src="/assets/luchaCancer.png"
+              src="/assets/Titulo1.jpeg"
               alt=""
               width={400}
               height={400}
@@ -35,10 +36,10 @@ const newsAndArticles = () => {
           </div>
         </div>
 
-        <div className="border rounded-lg overflow-hidden shadow-lg">
+        <div className="border rounded-lg overflow-hidden shadow-lg p-3">
           <div className="rounded-lg">
             <Image
-              src="/assets/luchaCancer.png"
+              src="/assets/Titulo2.jpeg"
               alt=""
               width={400}
               height={400}
@@ -60,10 +61,10 @@ const newsAndArticles = () => {
           </div>
         </div>
 
-        <div className="border rounded-lg overflow-hidden shadow-lg">
+        <div className="border rounded-lg overflow-hidden shadow-lg p-3">
           <div className="rounded-lg">
             <Image
-              src="/assets/luchaCancer.png"
+              src="/assets/Titulo3.jpeg"
               alt=""
               width={400}
               height={400}
@@ -85,10 +86,10 @@ const newsAndArticles = () => {
           </div>
         </div>
 
-        <div className="border rounded-lg overflow-hidden shadow-lg">
+        <div className="border rounded-lg overflow-hidden shadow-lg p-3">
           <div className="rounded-lg">
             <Image
-              src="/assets/luchaCancer.png"
+              src="/assets/Titulo4.jpeg"
               alt=""
               width={400}
               height={400}
@@ -112,8 +113,8 @@ const newsAndArticles = () => {
       </div>
 
       <div className="flex justify-center items-center">
-        <button className="bg-[#547EED] text-white w-1/6 rounded-full text-lg h-auto p-1">
-          Lee nuestras noticias y artículos
+        <button className="bg-[#547EED] hover:bg-[#3457b8] text-white py-2 px-6 rounded-full text-lg font-semibold h-auto p-1">
+          <Link href="/News">Lee nuestras noticias y artículos</Link>
         </button>
       </div>
     </div>
