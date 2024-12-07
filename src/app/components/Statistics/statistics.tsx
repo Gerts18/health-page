f87b882ee2ab58679cf1f0596b94d98c7babffaf
Carlos Gasca <l21121632@morelia.tecnm.mx>
2024-12-06 21:40:45 -0600

    Update statistics.tsx

--- src/app/components/Statistics/statistics.tsx
+++ src/app/components/Statistics/statistics.tsx
@@ -1,55 +1,68 @@
 import React from 'react';
-import { FaUserMd, FaVials, FaFolderOpen, FaFileAlt, FaTrophy, FaHeartbeat } from 'react-icons/fa';
+import { FaUserMd, FaHeartbeat } from 'react-icons/fa';
+import { RiMicroscopeFill } from "react-icons/ri";
+import { FaFolderPlus } from "react-icons/fa";
+import { FaFileMedical } from "react-icons/fa6";
+import { GrArticle } from "react-icons/gr";
+import { IoMdTrophy } from "react-icons/io";
 
 const Statistics = () => {
   const stats = [
     {
-      icon: <FaUserMd className="text-blue-500 text-5xl mb-4" />,
+      icon: <FaUserMd className="text-blue-500 text-5xl" />,
       number: '22,000',
       label: 'Pacientes evaluados',
     },
     {
-      icon: <FaVials className="text-blue-500 text-5xl mb-4" />,
+      icon: <RiMicroscopeFill className="text-blue-500 text-6xl" />,
       number: '92,000',
       label: 'Pruebas ejecutadas',
     },
     {
-      icon: <FaFolderOpen className="text-blue-500 text-5xl mb-4" />,
+      icon: <FaFolderPlus className="text-blue-500 text-5xl" />,
       number: '9,000',
       label: 'Muestras actuales en la biblioteca',
     },
     {
-      icon: <FaFileAlt className="text-blue-500 text-5xl mb-4" />,
+      icon: <FaFileMedical className="text-blue-500 text-5xl" />,
       number: '12,000',
       label: 'Muestras actuales en seroteca',
     },
     {
-      icon: <FaFileAlt className="text-blue-500 text-5xl mb-4" />,
+      icon: <GrArticle className="text-blue-500 text-6xl" />,
       number: '154',
       label: 'Artículos publicados en la última década',
     },
     {
-      icon: <FaTrophy className="text-blue-500 text-5xl mb-4" />,
+      icon: <IoMdTrophy className="text-blue-500 text-6xl" />,
       number: '+15',
       label: 'Grants nacionales e internacionales ejecutados',
     },
     {
-      icon: <FaHeartbeat className="text-blue-500 text-5xl mb-4" />,
+      icon: <FaHeartbeat className="text-blue-500 text-5xl" />,
       number: '+98,000',
       label: 'Años de vida ganados gracias al diagnóstico de precisión',
     },
   ];
 
   return (
-    <div className="bg-gray-50 py-12">
-      <h2 className="text-center text-3xl font-bold text-[#EB356E] mb-8">Cifras hablan por sí solas</h2>
-      <p className="text-center text-sm text-black">queda pendiente*</p>
-      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-6">
+    <div className="bg-gray-50 py-12 px-32">
+      <h2 className="text-center text-3xl font-bold text-[#EB356E] mb-8">
+        Cifras hablan por sí solas
+      </h2>
+      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {stats.map((stat, index) => (
-          <div key={index} className="flex flex-col items-center text-center">
-            {stat.icon}
-            <h3 className="text-3xl font-bold text-[#EB356E]">{stat.number}</h3>
-            <p className="text-gray-600 mt-2">{stat.label}</p>
+          <div
+            key={index}
+            className={`flex items-center p-6 ${
+              index === stats.length - 1 ? "flex justify-center" : ""
+            }`}
+          >
+            <div className="text-blue-500 text-5xl mr-4">{stat.icon}</div>
+            <div>
+              <h3 className="text-3xl font-bold text-[#EB356E]">{stat.number}</h3>
+              <p className="text-gray-600 mt-2">{stat.label}</p>
+            </div>
           </div>
         ))}
       </div>
