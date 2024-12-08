b96a8180fe620a9f4ec8a701131eba00bfda6d32
German <germants100@gmail.com>
2024-12-08 00:06:46 -0600

    fixing something in landing

--- src/app/components/BannerUniqueTreatment/bannerUniqueTreatment.tsx
+++ src/app/components/BannerUniqueTreatment/bannerUniqueTreatment.tsx
@@ -1,17 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const BannerUniqueTreatment: React.FC = () => {
   return (
-    <section className="flex flex-col  md:flex-row items-center bg-rose bg-white text-white h-[700px]">
+    <div className="flex flex-col md:flex-row items-center bg-white text-white h-[500px] my-2">
       {/* Imagen */}
-      <div className="flex absolute px-40">
+      <div className="flex-1 absolute px-40">
         <Image
-          src={"/assets/cancerHug.jpeg"}
-          width={700}
-          height={700}
+          src={"/assets/cancerHug.jpeg"} width={500} height={500}
           alt="Paciente y médico"
-          className="object-cover w-[700px] h-auto"
+          className="object-cover w-[500px] h-auto"
         />
       </div>
 
@@ -28,15 +27,16 @@ const BannerUniqueTreatment: React.FC = () => {
           ¡Cada persona es única como cada tratamiento es único!
         </h1>
         <p className="text-lg mb-6">
-          La medicina de precisión es el camino del futuro en el cuidado de la
-          salud. Descubre más sobre este emocionante enfoque visitando nuestro
-          canal de YouTube.
+          La medicina de precisión es el camino del futuro en el cuidado de la salud.
+          Descubre más sobre este emocionante enfoque visitando nuestro canal de YouTube.
         </p>
-        <button className="px-6 py-3 w-48 bg-[#EB356E] text-white rounded-3xl font-semibold hover:bg-pink-600 transition">
-          <a href="https://www.youtube.com/@fundacionficmac">Saber más</a>
-        </button>
+        <Link href='/about' >
+          <button className="px-6 py-3 w-48 bg-[#EB356E] text-white rounded-3xl font-semibold hover:bg-pink-600 transition">
+            Saber más
+          </button>
+        </Link>
       </div>
-    </section>
+    </div>
   );
 };
 
