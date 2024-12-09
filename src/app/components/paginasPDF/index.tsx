9a2163f06a26c7d6d4b40f3494aaf1803d9ef04f
German <germants100@gmail.com>
2024-12-09 13:46:12 -0600

    adding alt

--- src/app/components/paginasPDF/index.tsx
+++ src/app/components/paginasPDF/index.tsx
@@ -588,6 +588,7 @@ function PDFPage({ data }: PDFPageProps) {
           <Image 
             src="../assets/logo.png"
             style={styles.logoImage}
+            alt="Logo de la Clínica"
           />
         </View>
 
